ac4b23740eca456c90a60e8cf46b35e43c84bede
Hashi <rerenaga9170@icloud.com>
2024-12-04 17:18:29 +0100

    Implement database class with executing query method

--- database/sqliteBase.go
+++ database/sqliteBase.go
@@ -2,46 +2,39 @@ package database
 
 import (
 	"database/sql"
-	"fmt"
 	"go-sns/config"
 	"log"
-
-	"github.com/go-faker/faker/v4"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-var DbConnection *sql.DB
+type SqliteBase struct{
+	DbConnection *sql.DB
+}
 
-func init() {
+func NewSqliteBase() *SqliteBase{
 	var err error
-	DbConnection, err = sql.Open(config.Config.SQLDriver, config.Config.DbPath)
+	SqliteBase := &SqliteBase{}
+
+	SqliteBase.DbConnection, err = sql.Open(config.Config.SQLDriver, config.Config.DbPath)
 	if err != nil{
 		log.Fatalln(err)
 	}
-	defer DbConnection.Close()
 
-	stmt, err := DbConnection.Prepare("INSERT INTO posts (title, description) VALUES (?, ?)")
-	if err != nil{
-		log.Fatal(err)
+	return SqliteBase
+}
+
+func (s *SqliteBase) Execute(query string, args ...interface{}) error {
+	stmt, err := s.DbConnection.Prepare(query)
+	if err != nil {
+		return err
 	}
 	defer stmt.Close()
 
-	numRecords := 10
-	for i := 0; i < numRecords; i++{
-		title := faker.Paragraph()
-		description := faker.Paragraph()
-		
-		_, err := stmt.Exec(title, description)
-		if err != nil{
-			log.Fatal(err)
-		}
+	_, execErr := stmt.Exec(args...)
+	if execErr != nil {
+		return execErr
 	}
 
-	fmt.Println("Seeding Completed")
-
+	return nil
 }
-
-
-
-
 
