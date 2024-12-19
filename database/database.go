b1fd351673275ace625ad08299b484b003960a80
Hashi <rerenaga9170@icloud.com>
2024-12-19 16:52:48 +0100

    Change repositry structure

--- database/database.go
+++
@@ -1,8 +0,0 @@
-package database
-
-type Database interface {
-    GetTableLength(tableName string)(int, error)
-    PrepareAndExecute(query string, args ...interface{}) error
-    PrepareAndFetchAll(query string, args ...interface{}) ([]map[string]interface{}, error)
-    GetLastInsertedId() (int, error)
-}
